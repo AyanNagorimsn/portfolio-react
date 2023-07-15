a6dc93094993eb34f93ded204ac413c1fa0f7a4e
ParallelXL <ayanmsn07@gmail.com>
2023-07-15 17:25:31 +0530

    added navbar | default stylings |  assets

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+import { HiMenuAlt4, HiX } from "react-icons/hi";
+import "./Navbar.scss";
+import { images } from "../../constants";
+
+const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+  return (
+    <nav className="app__navbar">
+      <a href="#home" className="app__navbar-logo">
+        <img src={images.logoLight} alt="LOGO" />
+      </a>
+      <ul className="app__navbar-links">
+        {["home", "about", "work", "skills", "contact"].map((item) => (
+          <li className="app__flex p-text" key={`link-${item}`}>
+            <div></div>
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+
+      <div className="app__navbar-menu">
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+
+        <div className={`app__navbar-list ${toggle ? "active" : ""}`}>
+          <HiX onClick={() => setToggle(false)} />
+
+          <ul>
+            {["home", "about", "work", "skills", "contact"].map((item) => (
+              <li key={item}>
+                <div></div>
+                <a href={`#${item}`} onClick={() => setToggle(false)}>
+                  {item}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/constants/images.js
@@ -0,0 +1,50 @@
+import email from "../assets/email.png";
+import mobile from "../assets/mobile.png";
+import css from "../assets/css.png";
+import figma from "../assets/figma.png";
+import git from "../assets/git.png";
+import html from "../assets/html.png";
+import javascript from "../assets/javascript.png";
+import react from "../assets/react.png";
+import sass from "../assets/sass.png";
+
+import about01 from "../assets/about01.png";
+import about02 from "../assets/about02.png";
+import about03 from "../assets/about03.png";
+import about04 from "../assets/about04.png";
+
+import text from "../assets/text.png";
+import circle from "../assets/circle.svg";
+import logoLight from "../assets/logo-light.png";
+
+import bankist from "../assets/bankist.png";
+import pickgame from "../assets/pickgame.png";
+import logify from "../assets/logify.png";
+import oldportfolio from "../assets/oldportfolio.png";
+import guessthenum from "../assets/guessthenum.png";
+import weather from "../assets/weather.png";
+
+export default {
+  weather,
+  bankist,
+  pickgame,
+  logify,
+  oldportfolio,
+  guessthenum,
+  email,
+  mobile,
+  css,
+  figma,
+  git,
+  html,
+  javascript,
+  react,
+  sass,
+  about01,
+  about02,
+  about03,
+  about04,
+  text,
+  circle,
+  logoLight,
+};
